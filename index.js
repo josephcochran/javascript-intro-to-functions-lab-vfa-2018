410976b3aa474723eeee75c1af725200d9fd9354
Joseph Cochran <joseph.cochran@vanderbilt.edu>
2018-06-25 21:55:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,30 +26,3 @@ function sayHiToGrandma(string) {
  return("I love you, too.");
 }
  
- /* string.toLowerCase() === string;
-  string.toUpperCase() === string;
-  if (string=true)
-  return("I can't hear you!");
-  if (mixedcase=false)
-  return("I can't hear you!");
-  uppercase.toUpperCase() === uppercase;
-  if (uppercase=true)
-  return("YES INDEED!");
-  
-  var love = string;
-  if (love = "I love you, Grandma.")
-  return("I love you, too.");
-} */
-
-/*function sayHiToGrandma(string) {
-  var uppercase = "HELLO!"
-  uppercase.toUpperCase() === uppercase
-  if (uppercase=true)
-  return("YES INDEED!");
-}
-
-function sayHiToGrandma(string) {
-  var love = "I love you, Grandma."
-  if (love="I love you,Grandma.")
-  return("I love you, too.")
-}*/
